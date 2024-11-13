4498c7aef2753f9529f8d15abe09092bc46b8139
Mabdu11ah-abd <iamabdullahforu@gmail.com>
2024-11-13 16:41:48 +0500

    hello is it me your looking for

--- Lab_Task_3/server.js
+++ Lab_Task_3/server.js
@@ -1,8 +1,9 @@
-const express =  require("express");
-var expresslayouts = require("express-ejs-layouts");
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts"); // Make sure this matches throughout the code
+
 let server = express();
 server.set("view engine", "ejs");
-console.log(`HI`);
+
 server.use(expressLayouts);
 server.use(express.static("public"));
 
@@ -10,8 +11,9 @@ let adminProductsRouter = require("./routes/admin/products.controller");
 server.use(adminProductsRouter);
 
 server.get("/", (req, res) => {
-  return res.send(res.render("homepage"));
+  return res.send(res.render("HomePage"));
 });
+
 server.listen(5000, () => {
   console.log(`Server Started at localhost:5000`);
 });
