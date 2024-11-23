733646e8127bc3f540610eab890a486e0873f487
Mabdu11ah-abd <iamabdullahforu@gmail.com>
2024-11-23 18:31:23 +0500

    Lab task 3 complete

--- Lab_Task_3/routes/admin/products.controller.js
+++ Lab_Task_3/routes/admin/products.controller.js
@@ -4,9 +4,9 @@ let Product = require("../../models/product.model");
 
 router.get("/admin/products", async (req, res) => {
     let products = [{
-        title:"BOOK",
-        description: "BERY BIG BOOK",
-        price: 50,
+        title:"My title",
+        description: "My string",
+        price: 5,
     }]
     return res.render("admin/products", {
       layout: "adminlayout",
@@ -14,6 +14,7 @@ router.get("/admin/products", async (req, res) => {
       products,
     });
   });
+
   router.get("/admin/products/create", (req, res) => {
     return res.render("admin/product-form", { layout: "adminlayout" });
   });
@@ -24,10 +25,27 @@ router.get("/admin/products", async (req, res) => {
     newProduct.title = data.title;
     await newProduct.save();
     return res.redirect("/admin/products");
-    // we will send data to model to save in db
-  
-    // return res.send(newProduct);
-    // return res.render("admin/product-form", { layout: "adminlayout" });
   });
 
+router.get("/admin/products/delete/:id", async(req,res)=>{
+    let params = req.params;
+    let product = await Product.findByIdAndDelete(req.params.id);
+    return res.redirect("/admin/products");
+})
+
+router.get("/admin/products/edit/:id", async (req, res) => {
+    let product = await Product.findById(req.params.id);
+    return res.render("admin/product-edit-form", {
+      layout: "adminlayout",
+      product,
+    });
+});
+router.post("/admin/products/edit/:id", async (req, res) => {
+    let product = await Product.findById(req.params.id);
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.price = req.body.price;
+    await product.save();
+    return res.redirect("/admin/products");
+  });
 module.exports = router;
--- Lab_Task_3/server.js
+++ Lab_Task_3/server.js
@@ -7,16 +7,16 @@ server.set("view engine", "ejs");
 server.use(expressLayouts);
 server.use(express.static("public"));
 
+server.use(express.urlencoded());
 let adminProductsRouter = require("./routes/admin/products.controller");
 server.use(adminProductsRouter);
 
-server.use(express.urlencoded());
 server.get("/", (req, res) => {
-  return res.send(res.render("HomePage"));
+  return res.send(res.render("index2"));
 });
-server.get("/", (req,res)=>{
-  return res.send(res.render(""))
-})
+//admin panel is accessible at /admin/products
+
+//mongodb connection 
 mongoose.connect("mongodb+srv://iamabdullahforu:PsYR3xIqCaxlSZ5v@shanfoodsproject.gwpek.mongodb.net/?retryWrites=true&w=majority&appName=ShanFoodsProject")
 .then(() => {
   console.log("MongoDB connected successfully");
@@ -25,6 +25,7 @@ mongoose.connect("mongodb+srv://iamabdullahforu:PsYR3xIqCaxlSZ5v@shanfoodsprojec
   console.error("MongoDB connection failed:", err.message);
 })
 
+//starting server
 server.listen(5000, () => {
   console.log(`Server Started at localhost:5000`);
 });
